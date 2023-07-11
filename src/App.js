fe7a322bf32e08ffd2b04ee3158b7232ce18f1c4
Aryan Rai <aryanx23@pop-os.localdomain>
2023-07-11 21:12:45 +0530

    adding final changes for deployment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import MainPage from './MainPage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+        <div className="App">
+            <Routes>
+                <Route exact path="/personal-portfolio/" element={<MainPage />} />
+                <Route
+                    exact
+                    path="*"
+                    element={<Navigate to="/personal-portfolio/" replace={true} />}
+                />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
